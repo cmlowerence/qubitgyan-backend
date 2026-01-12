f99a10b6538dd9310bb050b6ca334c1bd0c03997
cmlowerence <cmlowerence2777@gmail.com>
2026-01-13 00:09:48 +0530

    update urls.py

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework.DefaultRouter import DefaultRouter
+from rest_framework.routers import DefaultRouter  # <--- FIXED IMPORT
 from .views import KnowledgeNodeViewSet, ResourceViewSet, ProgramContextViewSet
 
 router = DefaultRouter()
